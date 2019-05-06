ea0e0f24fa80536e4b4cbc40f798a0a3ab7685f6
HugoLipeng <1924949262@qq.com>
2019-05-06 21:41:55 +0800

    MoonCustomView

---
+++ MoonCustomView/src/main/java/com/android/hugo/mooncustomview/InvalidTextView.java
@@ -0,0 +1,42 @@
+package com.android.hugo.mooncustomview;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+
+/**
+ * Created by Administrator on 2016/4/4.
+ */
+public class InvalidTextView extends android.support.v7.widget.AppCompatTextView {
+    private Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+    public InvalidTextView(Context context) {
+        super(context);
+        initDraw();
+    }
+
+    public InvalidTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initDraw();
+    }
+
+    public InvalidTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initDraw();
+    }
+
+    private void initDraw() {
+        mPaint.setColor(Color.RED);
+        mPaint.setStrokeWidth((float) 1.5);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        int width = getWidth();
+        int height = getHeight();
+        canvas.drawLine(0, height / 2, width, height / 2, mPaint);
+    }
+}
